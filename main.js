54383c6260753946ccde7e98f695cac2e559166a
yasarba <veliyasar04@gmail.com>
2025-10-06 14:52:09 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,99 @@
+//variables
+let cookies = 0;
+let cookiesperclick = 1;
+let cookiespersecond = 0;
+
+//cookies per click upgrade data
+let cpcupgradecost = 10;
+let cpcupgradeamount = 1;
+
+//cookies per second upgrade data
+let cpsupgradecost = 100;
+let cpsupgradeamount = 10;
+
+//cookie button
+const cookie = document.getElementById("cookie")
+
+//data labels
+const cookiecounter = document.getElementById("cookiecounter");
+const cookiespersecondcounter = document.getElementById("cookiespersecond");
+const cookiesperclickcounter = document.getElementById("cookiesperclick");
+
+//upgrade buttons
+const upgradecookiesperclickbtn = document.getElementById("upgradecookiesperclick");
+const upgradecookiespersecondbtn = document.getElementById("upgradecookiespersecond");
+
+//saving data
+const localstorage = window.localstorage;
+
+//click the cookie - autoclicks once per second
+function clickcookie(cookieAmount) {
+  cookies += cookieAmount;
+  updatecookies();
+	
+};
+
+function autoclickcookie(){
+  clickcookie(Math.round(cookiespersecond / 100));
+  updatecookies();
+};
+
+//update all labels
+function updatecookies(){
+  cookiecounter.textContent = (cookies + " Cookies");
+  cookiespersecondcounter.textContent = (cookiespersecond + " Cookies Per Second");
+  cookiesperclickcounter.textContent = (cookiesperclick + " Cookie(s) Per Click");
+  upgradecookiesperclickbtn.textContent = ("Upgrade Cookies Per Click: " + cpcupgradecost + " Cookies");
+  upgradecookiespersecondbtn.textContent = ("Upgrade Cookies Per Second: " + cpsupgradecost + " Cookies");
+};
+
+//upgrade cookies per click
+function upgradecookiesperclick(){
+  
+  if (cookies >= cpcupgradecost){
+    cookies -= cpcupgradecost;
+    cookiesperclick += cpcupgradeamount;
+    
+    cpcupgradecost = (Math.round(cpcupgradecost*2.5))
+    
+    updatecookies();
+    
+  };
+  
+  
+};
+
+function upgradecookiespersecond(){
+  
+  if (cookies >= cpsupgradecost){
+    cookies -= cpsupgradecost;
+    cookiespersecond += cpsupgradeamount;
+    
+    cpsupgradecost = (Math.round(cpsupgradecost*2.5))
+    cpsupgradeamount = (Math.round(cpsupgradeamount*3))
+    
+    updatecookies();
+    autoclickcookie();
+  };
+  
+  
+  
+}
+
+
+//disable right-click
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+autoclickcookie();
+updatecookies();
+ 
+setInterval(autoclickcookie, 100);
+ 
+setInterval(updatecookies, 1000);
+
+setInterval(function(){
+    localStorage.setItem("cookies", cookies);
+    localStorage.setItem("cookiesperclick", cookiesperclick);
+    localStorage.setItem("cookiespersecond", cookiespersecond);
+    localStorage.setItem("cpcupgradecost", cpcupgradecost);
+    localStorage.setItem("cpsupgradecost", cpsupgradecost);
